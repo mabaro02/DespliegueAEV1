7ab19027b61c3d44afb3155b0d096c73b2528745
mabaro02 <mabaro02@floridauniversitaria.es>
2024-11-09 09:24:11 +0100

    Creacion de pagina de derrota del juego

---
+++ Directorio1/perder.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Derrota!</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <main>
+        <h1>¡Vuelve a intentarlo!</h1>
+        <p>¡Has perido!</p>
+        <button onclick="window.location.href='index.php'">Volver a empezar</button>
+    </main>
+</body>
+</html>
