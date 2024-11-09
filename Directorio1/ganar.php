01fb41838406c0a94f6ab49957895ac2366a4b87
mabaro02 <mabaro02@floridauniversitaria.es>
2024-11-09 09:23:47 +0100

    Creacion de pagina de victoria del juego

---
+++ Directorio1/ganar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Victoria!</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <main>
+        <h1>¡Felicidades, has ganado!</h1>
+        <p>¡Enhorabuena por completar el juego!</p>
+        <button onclick="window.location.href='index.php'">Volver a empezar</button>
+    </main>
+</body>
+</html>
